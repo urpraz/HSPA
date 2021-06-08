fe9488de51f190feba7993299961c33faa277ef5
Prasanna <urprasanna@gmail.com>
2021-06-08 23:12:45 +0530

    Second commit

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -9,6 +9,45 @@ export class PropertyListComponent implements OnInit {
 
   constructor() { }
 
+  properties: Array<any> = [
+    {
+      "Id": 1,
+      "Name":"Tata Birla House",
+      "Type":"House",
+      "Price":12000
+    },
+    {
+      "Id": 2,
+      "Name":"Birla House",
+      "Type":"House",
+      "Price":10000
+    },
+    {
+      "Id": 3,
+      "Name":"Tata House",
+      "Type":"House",
+      "Price":15000
+    },
+    {
+      "Id": 4,
+      "Name":"Eric House",
+      "Type":"House",
+      "Price":12000
+    },
+    {
+      "Id": 5,
+      "Name":"Erose House",
+      "Type":"House",
+      "Price":10000
+    },
+    {
+      "Id": 6,
+      "Name":"Lava House",
+      "Type":"House",
+      "Price":15000
+    }
+]
+
   ngOnInit(): void {
   }
 
